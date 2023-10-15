2c16a9ce3822cb7319374c5beaffe41f08d91c77
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-15 19:23:19 +0300

    Completed task

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,32 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+
+        result.add(firstQueue.element());
+        firstQueue.poll();
+        result.add(firstQueue.element());
+        firstQueue.poll();
+        result.add(secondQueue.element());
+        secondQueue.poll();
+        result.add(secondQueue.element());
+        secondQueue.poll();
+
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            firstQueue.add(result.getLast());
+            result.removeLast();
+            result.addLast(firstQueue.element());
+            firstQueue.remove();
+            result.addLast(firstQueue.element());
+            firstQueue.remove();
+
+            secondQueue.add(result.getLast());
+            result.removeLast();
+            result.addLast(secondQueue.element());
+            secondQueue.remove();
+            result.addLast(secondQueue.element());
+            secondQueue.remove();
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,27 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++){
+            queue.add(i);
+        }
+        int counter = 0;
+        while (!queue.isEmpty()){
+            Iterator<Integer> it = queue.iterator();
+            while (it.hasNext()){
+                Integer integer = it.next();
+                counter++;
+                if (counter % everyDishNumberToEat == 0){
+                    result.add(integer);
+                    it.remove();
+                }
+            }
+        }
+
+        return result;
     }
 }
